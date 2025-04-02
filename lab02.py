5951d5e923e4a9cdfd89a68f3aef980518f8ef24
Thành Chính <nguyennhuthanh03042006@gmail.com>
2025-04-02 20:39:12 +0700

    .

--- lab02.py
+++ lab02.py
@@ -1,42 +1,41 @@
-list = []
+danh_sach_sach = []
 
-def nhap_thong_tin_sach(list):
+def xep_loai_danh_gia(danh_gia):
+    if danh_gia == "5":
+        return "Tốt"
+    elif danh_gia == "4":
+        return "Khá Tốt"
+    elif danh_gia == "3":
+        return "Tốt"
+    elif danh_gia == "2":
+        return "Trung bình"
+    else:
+        return "Kém"
+
+def nhap_thong_tin_sach():
     
-    soLuong = int(input("Nhập số lượng sách thêm: "))
-    for i in range(0, soLuong):
+    so_luong = int(input("Nhập số lượng sách thêm: "))
+    for i in range(0, so_luong):
         print(f"Nhập thông tin sách: {i + 1}")
-        tenSach = input("Nhập tên sách: ")
-        tacGia = input("Nhập tên tác giả: ")
-        giaBan = input("Nhập giá sách: ")
-        danhGia = input("Nhập số sao(1/2/3/4/5): ")
-        
-        def tu_danh_gia(danhGia):
-            if danhGia == "5":
-                return "Tốt"
-            elif danhGia == "4":
-                return "Khá Tốt"
-            elif danhGia == "3":
-                return "Tốt"
-            elif danhGia == "2":
-                return "Trung bình"
-            else:
-                return "Kém"
-        
-        xepLoai = tu_danh_gia(danhGia)
+        ten_sach = input("Nhập tên sách: ")
+        tac_gia = input("Nhập tên tác giả: ")
+        gia_ban = input("Nhập giá sách: ")
+        danh_gia = input("Nhập số sao(1/2/3/4/5): ")
+        xep_loai = xep_loai_danh_gia(danh_gia)
           
-        Sach = {
-            "ten_sach" : tenSach,
-            "tac_gia" : tacGia,
-            "gia_ban" : giaBan,
-            "danh_gia" : danhGia,
-            "xep_loai" : xepLoai
+        thong_tin_sach = {
+            "ten_sach" : ten_sach,
+            "tac_gia" : tac_gia,
+            "gia_ban" : gia_ban,
+            "danh_gia" : danh_gia,
+            "xep_loai" : xep_loai
         }
-        list.append(Sach)
+        danh_sach_sach.append(thong_tin_sach)
         
-def hien_thi_danh_sach(list):
+def hien_thi_danh_sach():
     print("{:<15} {:<20}".format("","=====DANH SÁCH SÁCH====="))
     print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10}".format("STT","Tên sách","Tác giả","Giá sách","Đánh giá","Xếp loại"))
-    for i,sach in enumerate(list):
+    for i,sach in enumerate(danh_sach_sach):
         print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10}".format(i+1,sach.get("ten_sach", ""),sach.get("tac_gia", ""), sach.get("gia_ban", ""), sach.get("danh_gia", ""), sach.get("xep_loai")))
         
 while True:
@@ -48,9 +47,9 @@ while True:
     chon = input("Chọn chức năng: ")
     
     if chon == "1":
-        nhap_thong_tin_sach(list)
+        nhap_thong_tin_sach()
     if chon == "2":
-        hien_thi_danh_sach(list)
+        hien_thi_danh_sach()
     if chon == "3":
         break
         
